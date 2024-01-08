629b598a28b24acf538666aef090908e45b8dace
durga <durgag@sidgs.com>
2024-01-08 21:53:34 +0530

    update location

--- src/main/java/com/location/location/controller/LocationController.java
+++ src/main/java/com/location/location/controller/LocationController.java
@@ -5,6 +5,7 @@ import com.location.location.domain.City;
 import com.location.location.service.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ public class LocationController {
     @Autowired
     LocationService locationService;
 
+    @GetMapping("/getCities")
     public ResponseEntity<List<CinemaHall>> getCities() {
          return ResponseEntity.ok(locationService.getCities());
     }
